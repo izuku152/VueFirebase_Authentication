b083bc2582d85e736e5f08e415d885cf63ee9570
Izuku152e <hassanhanif123429@gmail.com>
2021-04-18 10:58:48 +0500

    commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCns4RT7JBWe0tcLmvxubLYe0JJfxnTTzw",
+  authDomain: "vue-firebase-ad486.firebaseapp.com",
+  projectId: "vue-firebase-ad486",
+  storageBucket: "vue-firebase-ad486.appspot.com",
+  messagingSenderId: "354269659313",
+  appId: "1:354269659313:web:99517d1aeff2a8bcdce290",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 createApp(App)
   .use(router)
   .mount("#app");
